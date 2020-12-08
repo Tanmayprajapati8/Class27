7f4b5b28ded01ff0ef208e608ef5fab878ba33f7
Tanmay <anjudeviaj2009@gmail.com>
2020-12-08 14:47:04 +0530

    created constraint

---
+++ chain.js
@@ -0,0 +1,21 @@
+class Chain {
+    constructor(body1,body2){
+        var options={
+            bodyA:body1,
+            bodyB:body2,
+            stiffness:0.04,
+            length:50
+        }
+        this.chain=Constraint.create(options);
+        World.add(world,this.chain);
+    }
+    display(){
+        push();
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.chain.bodyB.position;
+        strokeWeight(8);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+        pop();
+    }
+
+}
